94fa0c71216f54d55115a1e499c7f415665abb14
malys@LAPTOP-DSQPC4U1 <alenamalyseva422@gmail.com>
2022-08-03 01:25:59 +0300

    folder javascript added,first homework done

---
+++ Homework 01.08/scripts/script.js
@@ -0,0 +1,36 @@
+// Print to the console the result of dividing the number 100 by 4.
+let a = 100;
+let b = 4
+console.log(a / b);
+
+// Implement a program that evaluates and displays the value of the expression: 8 / 2 + 5 - -3 / 2
+let c = 8;
+let d = 5 ;
+let m = -3;
+console.log((c / 2) + d -(m / 2));
+
+// Print the following phrase to the console using concatenation: Lorem ipsum dolor sit amet
+let k = 'lorem ipsum';
+let l = 'dolor sit amet';
+console.log('concatenation:' + ' ' + k + ' ' + l);
+
+// You enter a phrase from multiple jobs in the console using interpolation.
+console.log(`interpolation: ${k} ${l}`)
+
+// Create a variable number and give it a value of 8. Then override the value -
+// set the new value to 15. Print the new value of the variable to the console.
+let x = 8;
+x = 15;
+console.log(x);
+
+// Create a variable sum that will perform the operation
+// of squaring the number 8. Print the variable sum to the console
+let sum = 8 ** 2;
+console.log(sum);
+
+// Create two variables: first name and last name. 
+// Give them arbitrary values. Print a line to the 
+// console in the format "First Name Last Name"
+let first_name = 'Alona';
+let lastName = 'Malysheva';
+console.log(`${first_name} ${lastName}`)
