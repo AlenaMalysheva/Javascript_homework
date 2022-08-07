4ae740846ae24ae34d42f752453a40f85e22a34f
malys@LAPTOP-DSQPC4U1 <alenamalyseva422@gmail.com>
2022-08-08 01:03:16 +0300

    Homework #2, 05.08

---
+++ Homework 05.08/scripts/script.js
@@ -0,0 +1,11 @@
+let user = ' Ivan Ivanov ';
+console.log(user.trim());
+
+let random_num = Math.random();
+let random_num2 = random_num.toFixed(3);
+console.log(random_num2);
+
+
+const text = 'Frontend developer';
+console.log('First:' + ' <'+ text[0] + '>' );
+console.log(`Last: <${text[text.length - 1]}>`);
